754f226ddd15b21de75e490bd401ad20b3a0fd47
jahangirripon <jripon1991@gmail.com>
2025-02-14 18:54:30 +0600

    Added open/close state in steps component

--- src/Steps.js
+++ src/Steps.js
@@ -6,7 +6,8 @@ export default function Steps() {
   // const step = 2;
 
   const [step, setStep] = useState(1);
-  console.log([step, setStep]);
+  const [isOpen, setIsOpen] = useState(true);
+  // console.log([step, setStep]);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -16,31 +17,43 @@ export default function Steps() {
     if (step < 3) setStep(step + 1);
   }
 
+  function handleClose() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step: {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950F2", color: "#fff" }}
-          onClick={handlePrevious}
-          //   onMouseOver={() => alert("hi")}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950F2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step: {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950F2", color: "#fff" }}
+              onClick={handlePrevious}
+              //   onMouseOver={() => alert("hi")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950F2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
