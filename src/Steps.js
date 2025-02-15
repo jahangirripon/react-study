e9ac7469541ea82126aa8710dd61a705fb5d8123
jahangirripon <jripon1991@gmail.com>
2025-02-15 09:17:37 +0600

    updating state const with callback function

--- src/Steps.js
+++ src/Steps.js
@@ -10,11 +10,12 @@ export default function Steps() {
   // console.log([step, setStep]);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    // if (step > 1) setStep(s - 1);
+    if (step > 1) setStep((s) => s - 1); // use callback function is safer
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   function handleClose() {
