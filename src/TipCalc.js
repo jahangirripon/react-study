92be17967e8eea76014692da8b6f8f70af98bf7d
jahangirripon <jripon1991@gmail.com>
2025-03-23 19:34:04 +0600

    created logic for Tip calculator

--- src/TipCalc.js
+++ src/TipCalc.js
@@ -2,75 +2,81 @@ import { useState } from "react";
 
 export default function TipCalc({ onAddItems }) {
   const [bill, setBill] = useState("");
-  const [myRating, setMyRating] = useState("");
-  const [friendRating, setFriendRating] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e);
-    // if (!description) return;
-    // const newItem = { description, quantity, packed: false, id: Date.now() };
-    // console.log(newItem);
-    // onAddItems(newItem);
-    // handleAddItems(newItem);
-    // setDescription("");
-    // setQuantity(1);
+  const [myRating, setMyRating] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
+  const tip = (bill * (myRating + friendRating)) / 2 / 100;
+  function handleReset() {
+    setBill("");
+    setMyRating(0);
+    setFriendRating(0);
   }
+
   return (
     <div>
-      <form className="add-form" onSubmit={handleSubmit}>
-        <div>
-          <p> Bill...</p>
-          <input
-            type="text"
-            placeholder="Bill..."
-            value={bill}
-            onChange={(e) => setBill(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <p> Your Rating</p>
-          <select
-            value={myRating}
-            onChange={(e) => setMyRating(Number(e.target.value))}
-          >
-            <option value={0} key={0}>
-              Not Satisfied - 0%
-            </option>
-            <option value={5} key={5}>
-              Okay - 5%
-            </option>
-            <option value={10} key={10}>
-              Good - 10%
-            </option>
-            <option value={20} key={20}>
-              Very Satisfied - 20%
-            </option>
-          </select>
-        </div>
-
-        <div>
-          <p> Your Friends Rating</p>
-          <select
-            value={friendRating}
-            onChange={(e) => setFriendRating(Number(e.target.value))}
-          >
-            <option value={0} key={0}>
-              Not Satisfied - 0%
-            </option>
-            <option value={5} key={5}>
-              Okay - 5%
-            </option>
-            <option value={10} key={10}>
-              Good - 10%
-            </option>
-            <option value={20} key={20}>
-              Very Satisfied - 20%
-            </option>
-          </select>
-        </div>
-      </form>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={myRating} onSelect={setMyRating}>
+        Your Rating:
+      </SelectPercentage>
+      <SelectPercentage percentage={friendRating} onSelect={setFriendRating}>
+        Your Friends Rating:
+      </SelectPercentage>
+      {bill > 0 && (
+        <>
+          <OutPut bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
+
+  function BillInput({ bill, onSetBill }) {
+    return (
+      <div>
+        <label>Bill::</label>
+        <input
+          type="text"
+          placeholder="Bill..."
+          value={bill}
+          onChange={(e) => onSetBill(Number(e.target.value))}
+        />
+      </div>
+    );
+  }
+
+  function SelectPercentage({ children, percentage, onSelect }) {
+    return (
+      <div>
+        <label>{children}</label>
+        <select
+          value={percentage}
+          onChange={(e) => onSelect(Number(e.target.value))}
+        >
+          <option value={0} key={0}>
+            Not Satisfied - 0%
+          </option>
+          <option value={5} key={1}>
+            Okay - 5%
+          </option>
+          <option value={10} key={2}>
+            Good - 10%
+          </option>
+          <option value={20} key={3}>
+            Very Satisfied - 20%
+          </option>
+        </select>
+      </div>
+    );
+  }
+
+  function OutPut({ bill, tip }) {
+    return (
+      <h3>
+        You pay ${bill + tip} ({bill}$ + ${tip} tip)
+      </h3>
+    );
+  }
+
+  function Reset({ onReset }) {
+    return <button onClick={onReset}>Reset</button>;
+  }
 }
